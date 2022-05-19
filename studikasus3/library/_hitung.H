a7d124d3b4663208960a8acf68080c0239f415be
fahrulsanaky <101966278+fahrulsanaky@users.noreply.github.com>
2022-05-19 15:55:15 +0900

    Add files via upload

---
+++ studikasus3/library/_hitung.H
@@ -0,0 +1,11 @@
+using namespace std;
+class Hitung {
+	friend ostream& operator<<(ostream&, const Hitung&);
+	friend istream& operator>>(istream&, Hitung&);
+	public:
+	Hitung();
+	void hitung_jumlahnya(){ jumlah = (a + b + c); }
+	private:
+	int a,b,c;
+	int jumlah;
+};
